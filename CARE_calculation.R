c77d830a63320e4d735bf7efcfa0b68663cf2528
ShuyaCui <126207792+ShuyaCui@users.noreply.github.com>
2023-02-23 17:51:24 -0500

    Update CARE_calculation.R

--- CARE_calculation.R
+++ CARE_calculation.R
@@ -14,11 +14,14 @@ npar_act = as.data.frame(npar_act)
 
 # calculate CARE by SSA
 ssa_care0 = c()
+ssa_behavioral_energy = c()
 for (i in 1:length(act_data_list)){
     act = act_data_list[[i]]
     # SSA circadian component
     s.act = ssa(act$activity, L = 1440, kind = "1d-ssa")
     ssa_care0[i] = sum(contributions(s.act)[2:3])
+   # energy of behavioral noise
+    ssa_behavioral_energy = sum(contributions(s.act)[4:length(contributions(s.act))])
 }
 # calibrate CARE_0 by gamma, here gamma = scaled maximum activity intensity
 gamma = npar_act$M10/max(npar_act$M10, na.rm = T)
