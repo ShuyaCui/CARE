8c1591ab9d320d74b3c00b64792b112505ab877b
ShuyaCui <126207792+ShuyaCui@users.noreply.github.com>
2023-03-14 10:26:42 -0400

    Update CARE_calculation.R

--- CARE_calculation.R
+++ CARE_calculation.R
@@ -23,10 +23,6 @@ for (i in 1:length(act_data_list)){
    # energy of behavioral noise
     ssa_behavioral_energy = sum(contributions(s.act)[4:length(contributions(s.act))])
 }
-# calibrate CARE_0 by gamma, here gamma = scaled maximum activity intensity
-gamma = npar_act$M10/max(npar_act$M10, na.rm = T)
-ssa_care1 = ssa_care0/gamma
 
 write.csv(ssa_care0, file="./results/CARE_0.csv", row.names=F)
-write.csv(ssa_care1, file="./results/CARE_1.csv", row.names=F)
 write.csv(npar_act, file="./results/nonparametric_variables.csv", row.names=F)
