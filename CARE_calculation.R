ee766a9882f5f74f2bda43af6fbfd3ca8201181b
ShuyaCui <126207792+ShuyaCui@users.noreply.github.com>
2023-04-12 16:34:35 -0400

    Update CARE_calculation.R

--- CARE_calculation.R
+++ CARE_calculation.R
@@ -19,9 +19,15 @@ for (i in 1:length(act_data_list)){
     act = act_data_list[[i]]
     # SSA circadian component
     s.act = ssa(act$act, L = 1440, kind = "1d-ssa")
-    ssa_care0[i] = sum(contributions(s.act)[2:3])
+    idx_24hr = c()
+    for(j in 2:length(contributions(s.act))){
+         if(abs(period_estimate(s.act,j)-1440) < 10){
+           idx_24hr = c(idx_24hr, j)
+         }
+    }
+    ssa_care0[i] = sum(contributions(s.act)[idx_24hr])
    # energy of behavioral noise
-    ssa_behavioral_energy[i] = sum(contributions(s.act)[4:length(contributions(s.act))])
+    ssa_behavioral_energy[i] = sum(contributions(s.act)[-c(1,idx_24hr)])
 }
 res = cbind(ssa_care0, ssa_behavioral_energy, npar_act)
 res = as.data.frame(res)
